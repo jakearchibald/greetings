747c5957a8c69bd1ab4666de2438dc0f67123436
Jake Archibald <jaffathecake@gmail.com>
2012-09-23 18:08:03 +0100

    Simplification + newline at the end

--- setup.js
+++ setup.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
+var mode = process.argv[2];
+var args = process.argv.slice(3);
 
 if ( process.argv[2] == 'add' ) {
-	process.argv.slice(3).forEach(function (val) {
+	args.forEach(function (val) {
 		fs.writeFile( val + '.txt', "Add a greeting below this line:\n" );
 	});
 }
 else if ( process.argv[2] == 'trick' ) {
-	process.argv.slice(3).forEach(function (val) {
-		fs.appendFile( val + '.txt', "Oh, and above this line, please. *trollface*" );
+	args.forEach(function (val) {
+		fs.appendFile( val + '.txt', "Oh, and above this line, please. *trollface*\n" );
 	});
 }
 else {

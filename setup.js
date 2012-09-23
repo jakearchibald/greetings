8ebc91309a165c0ec4ea835e39d70ef753d4e5bd
Jake Archibald <jaffathecake@gmail.com>
2012-09-23 18:10:45 +0100

    Using mode arg

--- setup.js
+++ setup.js
@@ -2,12 +2,12 @@ var fs = require('fs');
 var mode = process.argv[2];
 var args = process.argv.slice(3);
 
-if ( process.argv[2] == 'add' ) {
+if ( mode == 'add' ) {
 	args.forEach(function (val) {
 		fs.writeFile( val + '.txt', "Add a greeting below this line:\n" );
 	});
 }
-else if ( process.argv[2] == 'trick' ) {
+else if ( mode == 'trick' ) {
 	args.forEach(function (val) {
 		fs.appendFile( val + '.txt', "Oh, and above this line, please. *trollface*\n" );
 	});
